43722cfa9a13d05c73cdac2977bab11fd986b7e0
Ricky Rivera <ricky.rivera@shipstation.com>
2020-08-13 17:04:35 -0500

    change melpa source

--- packages.el
+++ packages.el
@@ -8,7 +8,7 @@
 (setq package-enable-at-startup nil)
 
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
